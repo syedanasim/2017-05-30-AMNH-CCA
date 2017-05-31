21bd3f803dade1e618d852dc277f0db61370355a
SyedaN <snasim895@gmail.com>
2017-05-31 11:26:10 -0400

    initial notes

---
+++ python_practice/practice_script.py
@@ -0,0 +1,5 @@
+'''
+This script contains an example class that we will write. The class will be be called algebra, and it will contain some simple methods to do some algebraic manipulation.
+
+The script will end with 
+'''
